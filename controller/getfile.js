4bbb6f477f0284e2e30695061cafbbd537c1fb67
Francisco G <francisco.asevero@gmail.com>
2021-06-14 23:50:49 -0300

    Adicionado try-catch e estruturado com json o retorno

--- controller/getfile.js
+++ controller/getfile.js
@@ -1,18 +1,35 @@
 const axios = require("axios");
-
 /**
  * 
  * @param {string} urlParams 
+ * @returns {Data Json}
  */
-async function getFileUrl(urlParams) {
+async function getFileUrl() {
     try{
-	const request = await axios.get(urlParams)
-    console.log("Código de Estado: " ,request.status)
-    return request.data
-    }catch(err){
-        console.log("Código de Estado: " ,err.response.status)
-        return err.response.status
+	    const request = await axios.get("https://sevgo.com.br/robots.txt")
+        //return request.data
+        //console.log("Código de Estado: " ,request)
+
+        if (request.status === 200) {
+            return request.status(200).json({
+                success: true,
+                status: 200,
+                data: request,
+                message: "Transação realizada com sucesso"
+            });
+        }
+        //console.log("Falha Na Transação")
+        return resquest.status(404).json({
+            success: false,
+            status: 404,
+            data: request,
+            message: "Falha na transação"
+        });
+    }catch(error){
+        //console.log("Falha Na Transação - Error")
+        return error.response
     }
 }
+
 
 getFileUrl()
